21eae1d03b26c0d3529734b7d0308de5ba615d65
Ahmet NİZAM <ahmetnizam@gmail.com>
2024-02-27 14:30:40 +0300

    repository temizlendi

--- params.php
+++
@@ -1,5 +0,0 @@
-<?php
-	error_reporting(0);
-
-	$_PARAM['ldap_attribute'] = array("displayname", "sn", "mail", "title", "department", "physicaldeliveryofficename", "manager"); //array("memberof", "mail", "sn", "cn");
-?>
